ef06009da3deb1445a405298ccdc9329846596aa
Ryan Duff <ryanduff613@gmail.com>
2020-05-26 15:34:39 -0700

    update songs_controller.rb with method for create and private method song_params

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,3 +5,18 @@ class SongsController < ApplicationController
     render :new
   end
 end
+
+def create 
+  @album = Album.find(params[:album_id])
+  @song = @album.songs.new(song_params)
+  if @song.save
+    redirect_to album_path(@album)
+  else
+    render :new
+  end
+end
+
+private
+def song_params
+  params.require(:song).permit(:name, :lyrics)
+end
